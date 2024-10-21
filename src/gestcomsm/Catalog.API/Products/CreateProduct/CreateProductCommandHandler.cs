5388c59a877f8aa91897903cc2c76289a05905e3
SMuxili1 <ramiromuxili.mwangobrain@outlook.pt>
2024-10-22 00:19:38 +0100

    feat(BuildingBlocks): ValidationBehavior.

--- src/gestcomsm/Catalog.API/Products/CreateProduct/CreateProductCommandHandler.cs
+++ src/gestcomsm/Catalog.API/Products/CreateProduct/CreateProductCommandHandler.cs
@@ -4,7 +4,19 @@
         : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
 
-    internal class CreateProductCommandHandler(IDocumentSession session, ILogger<CreateProductCommandHandler> logger) 
+    public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommandValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+            RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required");
+            RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is required");
+            RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
+        }
+    }
+
+    internal class CreateProductCommandHandler(IDocumentSession session, 
+        IValidator<CreateProductCommand> validator, ILogger<CreateProductCommandHandler> logger) 
         : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
@@ -12,6 +24,14 @@
             logger.LogInformation("CreateProductCommandHandler.Handle called with {@Command}", command);
 
             //create Product entity from command object
+
+            var result = await validator.ValidateAsync(command, cancellationToken);
+            var errors = result.Errors.Select(x => x.ErrorMessage).ToList();
+            if (errors.Any())
+            {
+                throw new ValidationException(errors.FirstOrDefault());
+            }
+
             var product = new Product
             {
                 Name = command.Name,
--- src/gestcomsm/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
+++ src/gestcomsm/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -1,7 +1,7 @@
 ﻿namespace Catalog.API.Products.DeleteProduct
 {
     //public record DeleteProductRequest(Guid Id);
-    public record DeleteProductResponse(bool isSuccess);
+    public record DeleteProductResponse(bool IsSuccess);
     public class DeleteProductEndpoint : ICarterModule
     {
         public void AddRoutes(IEndpointRouteBuilder app)
